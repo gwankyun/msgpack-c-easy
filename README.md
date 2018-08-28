d967d2856c6af600c2c255abf0f194c66d0c604a
gwankyun <ljqican@163.com>
2018-08-29 00:15:38 +0800

    添加代碼，修改說明

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # msgpack-c-easy
+
+依賴[msgpack/msgpack-c](https://github.com/msgpack/msgpack-c)庫。
+在msgpack::easy命名空間內引入：
+```
+template<typename T>
+std::string pack(const T& t)
+
+template<typename T>
+T unpack(std::string str)
+```
---
+++ msgpack_easy.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef MSGPACK_EASY
+#define MSGPACK_EASY
+#include <string>
+#include <sstream>
+#include <msgpack.hpp>
+
+namespace msgpack
+{
+	namespace easy
+	{
+		template<typename T>
+		std::string pack(const T& t)
+		{
+			using namespace std;
+			stringstream buffer;
+			msgpack::pack(buffer, t);
+			buffer.seekg(0);
+			return string(buffer.str());
+		}
+
+		template<typename T>
+		T unpack(std::string str)
+		{
+			return msgpack::unpack(str.data(), str.size()).get().as<T>();
+		}
+	}
+}
+#endif // !MSGPACK_EASY
